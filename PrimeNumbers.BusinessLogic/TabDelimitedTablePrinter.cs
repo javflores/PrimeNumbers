eb15a3662982cf8bcc02145f61b0ef3950e3a683
lucianbornaz <b9H1W4Fz>
2015-10-31 00:44:30 +0000

    Implemented TabDelimitedTablePrinter

--- PrimeNumbers.BusinessLogic/TabDelimitedTablePrinter.cs
+++ PrimeNumbers.BusinessLogic/TabDelimitedTablePrinter.cs
@@ -1,12 +1,58 @@
-﻿namespace PrimeNumbers.BusinessLogic
-{
-    public class TabDelimitedTablePrinter
-    {
-        public static string Tab = "\t";
+﻿using System;
+using System.Text;
 
-        public void Print(PrimeTable primeTable)
+namespace PrimeNumbers.BusinessLogic
+{
+    public class TabDelimitedTablePrinter : ITablePrinter
+    {
+        public const string Tab = "\t";
+
+        public void Print(PrimeTable table)
         {
-            throw new System.NotImplementedException();
+            if (table == null || table.AxisX == null)
+                return;
+
+            PrintHeader(table);
+            for (var i = 0; i < table.AxisX.Length; i++)
+            {
+                PrintRow(table, i);
+            }
+        }
+
+        private void PrintHeader(PrimeTable table)
+        {
+            var builder = new StringBuilder();
+            builder.Append(Tab);
+
+            for (var i = 0; i < table.AxisX.Length; i++)
+            {
+                builder.Append(table.AxisX[i]);
+
+                if ((i + 1) < table.AxisX.Length)
+                    builder.Append(Tab);
+            }
+
+            var output = builder.ToString();
+            Console.WriteLine(output);
+        }
+
+        private void PrintRow(PrimeTable table, int index)
+        {
+            var builder = new StringBuilder();
+            builder.Append(table.AxisY[index]);
+            builder.Append(Tab);
+
+            for (var i = 0; i < table.AxisX.Length; i++)
+            {
+                var cellValue = table.GetCellValue(index, i);
+                builder.Append(cellValue);
+
+                if ((i + 1) < table.AxisX.Length)
+                    builder.Append(Tab);
+            }
+
+            var output = builder.ToString();
+            Console.WriteLine(output);
         }
     }
 }
---
+++ PrimeNumbers.Unit.Tests/TabDelimitedTablePrinterTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+using PrimeNumbers.BusinessLogic;
+
+namespace PrimeNumbers.Unit.Tests
+{
+    [TestFixture]
+    public class TabDelimitedTablePrinterTest
+    {
+        private ITablePrinter _tablePrinter;
+        private StringWriter _consoleOutWriter;
+
+        [SetUp]
+        public void Setup()
+        {
+            _consoleOutWriter = new StringWriter();
+            Console.SetOut(_consoleOutWriter);
+
+            _tablePrinter = new TabDelimitedTablePrinter();
+        }
+
+        [Test]
+        public void Should_Not_Print_Null_Prime_Table()
+        {
+            //given
+            var expectedOutput = String.Empty;
+            PrimeTable table = null;
+
+            //when
+            _tablePrinter.Print(table);
+
+            //then
+            var consoleOutput = _consoleOutWriter.ToString();
+            Assert.AreEqual(expectedOutput, consoleOutput);
+        }
+
+        [Test]
+        public void Should_Not_Print_Empty_Prime_Table()
+        {
+            //given
+            var expectedOutput = String.Empty;
+            PrimeTable table = new PrimeTable(null);
+
+            //when
+            _tablePrinter.Print(table);
+
+            //then
+            var consoleOutput = _consoleOutWriter.ToString();
+            Assert.AreEqual(expectedOutput, consoleOutput);
+        }
+
+        [Test]
+        public void Should_Print_Prime_Table_Rank_3()
+        {
+            //given
+            var expectedOutput = GetExpectedOutputForRank3Table();
+
+            var tableAxis = new[] { 2, 3, 5 };
+            var table = new PrimeTable(tableAxis);
+
+            //when
+            _tablePrinter.Print(table);
+
+            //then
+            var consoleOutput = _consoleOutWriter.ToString();
+            Assert.AreEqual(expectedOutput, consoleOutput);
+        }
+
+        private string GetExpectedOutputForRank3Table()
+        {
+            var raw1 = String.Format("{0}2{0}3{0}5{1}", TabDelimitedTablePrinter.Tab, Environment.NewLine);
+            var raw2 = String.Format("2{0}4{0}6{0}10{1}", TabDelimitedTablePrinter.Tab, Environment.NewLine);
+            var raw3 = String.Format("3{0}6{0}9{0}15{1}", TabDelimitedTablePrinter.Tab, Environment.NewLine);
+            var raw4 = String.Format("5{0}10{0}15{0}25{1}", TabDelimitedTablePrinter.Tab, Environment.NewLine);
+
+            return raw1 + raw2 + raw3 + raw4;
+        }
+    }
+}
